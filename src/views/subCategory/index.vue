8bbde706b2b0f0e9e960bde9af78aa49b2c92e07
TurnArk <1716236031@qq.com>
2024-11-02 17:54:27 +0800

    二级分类-面包屑导航

--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -1,4 +1,19 @@
 <script setup>
+import {getCategoryFilterAPI} from "@/apis/category.js";
+import {ref,onMounted} from "vue";
+import {useRoute} from "vue-router";
+
+const categoryData=ref([])
+const route=useRoute()
+async function getCategory(){
+  const res=await getCategoryFilterAPI(route.params.id)
+  categoryData.value=res.result
+  console.log(categoryData.value)
+}
+
+onMounted(()=>{
+  getCategory()
+})
 
 
 </script>
@@ -9,9 +24,9 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
+        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{categoryData.parentName}}
         </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
