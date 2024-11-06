63f2c5fc10bd0ce370db3f419d96a5c573231cfa
TurnArk <1716236031@qq.com>
2024-11-06 14:03:50 +0800

    详情-热榜-基础组件封装和基础数据获取渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import {getDetail} from "@/apis/detail.js";
 import {ref,onMounted} from "vue";
 import {useRoute} from "vue-router";
+import DetailHot from "@/views/Detail/components/DetailHot.vue";
 
 const route=useRoute()
 const goods =ref([])
@@ -117,7 +118,8 @@ onMounted(()=>{
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <DetailHot/>
+              <DetailHot/>
             </div>
           </div>
         </div>
