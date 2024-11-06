eda9159ed0f653ceff2bff65bb0c1ab04b1451bc
TurnArk <1716236031@qq.com>
2024-11-06 15:36:25 +0800

    放大镜效果-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {ref} from 'vue'
+import {ref,watch} from 'vue'
+import {useMouseInElement} from "@vueuse/core";
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -10,10 +11,34 @@ const imageList = [
 ]
 
 const activeIndex=ref(0)
-
 const enterHandler=(i)=>{
   activeIndex.value=i
 }
+
+const target=ref(null)
+const {elementX,elementY,isOutside}=useMouseInElement(target)
+const left=ref(0)
+const top=ref(0)
+watch([elementX,elementY],()=>{
+  if(elementX.value>100&&elementX.value<300){
+    left.value=elementX.value-100
+  }
+  if(elementY.value>100&&elementY.value<300){
+    top.value=elementY.value-100
+  }
+  if(elementX>300){
+    left.value=200
+  }
+  if(elementY>300){
+    top.value=200
+  }
+  if(elementX<100){
+    left.value=0
+  }
+  if(elementY<100){
+    top.value=0
+  }
+})
 </script>
 
 
@@ -23,7 +48,7 @@ const enterHandler=(i)=>{
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
